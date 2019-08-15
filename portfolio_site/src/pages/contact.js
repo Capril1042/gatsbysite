6e72e5ee3c96896a1248d5b8fe11837d68b0b320
Capril1042 <Caprilewis1042@gmail.com>
2019-08-15 13:24:53 -0400

    adds contact info

--- portfolio_site/src/pages/contact.js
+++ portfolio_site/src/pages/contact.js
@@ -7,7 +7,18 @@ import SEO from "../components/seo"
 const ContactPage = () => (
   <Layout>
     <SEO title="Contact" />
-    <h1>Contact Page</h1>
+    <h1>General Information</h1>
+    <p>I love talking with developers 
+      and people learning to code! 
+      Please feel free to reach out to me :)
+      <br/>
+      Twitter and email are the best ways to reach me.
+      </p>
+    <h1>Contact Details</h1>
+    <ul> 
+      <li>email: frankdelpidio@gmail.com</li>
+      <li>twitter: @fdel15 </li>
+    </ul>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
